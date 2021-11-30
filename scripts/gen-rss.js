1324ba1d266bb724a82911e0a16c3b91481fd19b
bornforlamp <79363411+bornforlamp@users.noreply.github.com>
2021-11-30 14:18:54 +0530

    Update gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Harshad Mane',
+    site_url: 'https://harshadmane.in',
+    feed_url: 'https://harshadmane.in/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
