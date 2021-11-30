5cbdafdb4ace9d3ac0747a50e5a395e60ac49a03
bornforlamp <79363411+bornforlamp@users.noreply.github.com>
2021-11-30 16:14:29 +0530

    Create next.config.js

---
+++ pages/next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/hello-vercel',
+        destination: 'https://vercel.com',
+        permanent: true,
+      },
+    ]
+  },
+}
