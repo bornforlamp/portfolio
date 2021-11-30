32e5de060fac552b5828f0f13e9bbb12502cbd1a
bornforlamp <79363411+bornforlamp@users.noreply.github.com>
2021-11-30 15:23:50 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,2 +1,12 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
-module.exports = withNextra()
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/photos',
+        destination: 'https://vercel.com',
+        permanent: true,
+      },
+    ]
+  },
+}
